cdb067a1216436c5df354675136f0b221d7868d2
uriellopes <urielrlps@gmail.com>
2020-03-21 12:11:14 -0300

    Wip Salvar Progresso

--- Projeto_Batalha_Naval/src/batalha_naval/Jogador.java
+++ Projeto_Batalha_Naval/src/batalha_naval/Jogador.java
@@ -6,12 +6,20 @@ public class Jogador {
 	private String nome;
 	private Tabuleiro tabuleiro;
 	private Random rand = new Random();
-	private Navio navios[] = new Navio[] {new Navio("Porta-aviões", 5, rand.nextBoolean()),new Navio("Porta-aviões", 5, rand.nextBoolean()),new Navio("Porta-aviões", 5, rand.nextBoolean()),new Navio("Porta-aviões", 5, rand.nextBoolean()),new Navio("Porta-aviões", 5, rand.nextBoolean())};
-	private int qtd_navios = 5;
+	private int qtd_navios;
+	private Navio navios[];
 	
 	public Jogador(String n) {
 		this.nome = n;
 		this.tabuleiro = new Tabuleiro();
+		this.qtd_navios = 5;
+		this.navios = new Navio[]	{
+										new Navio("Porta-aviões", 5, this.rand.nextBoolean()),
+										new Navio("Navio de guerra", 4, this.rand.nextBoolean()),
+										new Navio("Cruzador", 3, this.rand.nextBoolean()),
+										new Navio("Submarino", 3, this.rand.nextBoolean()),
+										new Navio("Destruidor", 2, this.rand.nextBoolean())
+									};
 		this.tabuleiro.randomizarNavios(navios);
 	}
 	
